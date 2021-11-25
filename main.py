ee4bcb3887373cb21e8177ec9ef2fd573503793a
leand12 <leandrosilva12@ua.pt>
2021-11-25 18:51:07 +0000

    vertice as coordinates

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
-import random
 import itertools
+import random as rdm
 import networkx as nx
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from timeit import default_timer as timer
+from time import perf_counter
+from typing import Tuple
+
+Point = Tuple[int, int]
 
 
 class SearchGraph:
@@ -14,7 +17,7 @@ class SearchGraph:
         self.edges = {}
         self.min_cost = None
         self.min_subsets = None
-        self.time = None
+        self.exec_time = None
 
     @property
     def size(self):
@@ -24,14 +27,15 @@ class SearchGraph:
     def solution(self):
         return self.min_cost, self.min_subsets
 
-    def add_vertice(self, v: int, w: int):
-        assert w >= 0, "Weight must be positve"
+    def add_vertice(self, v: Point, w: int):
+        assert w >= 0, "Weight must be positive"
+        assert isinstance(v, tuple), f"Vertice must be a tuple"
         assert v not in self.vertices, f"Vertice {v} already added"
 
         self.vertices[v] = w
         return self
 
-    def add_edge(self, v1: int, v2: int):
+    def add_edge(self, v1: Point, v2: Point):
         for v in (v1, v2):
             assert v in self.vertices, f"Vertice {v} does not exist"
         assert not v1 == v2, f"Vertices {v1} {v2} are equal"
@@ -41,15 +45,18 @@ class SearchGraph:
         return self
 
     def read_graph(self, filename):
+        self.vertices.clear()
+        self.edges.clear()
 
         try:
             file_ = open(filename, "r")
         except FileNotFoundError:
             print(f"ERR: File \"{filename}\" not found")
+            exit(1)
 
         l = 0
         while file_:
-            line = file_.readline().strip()
+            line = file_.readline()
             l += 1
             if not line:
                 continue
@@ -57,33 +64,49 @@ class SearchGraph:
                 num_vertices = int(line)
             except:
                 print(f"ERR: Expected a number on line {l}")
+                exit(1)
 
+            vertices = []
             for v in range(num_vertices):
-                weight, *neighbours = file_.readline().strip().split()
-                l += 1
-
                 try:
-                    weight = int(weight)
-                    neighbours = [int(n) for n in neighbours]
+                    x, y, weight, *neighbours = [int(w)
+                                                 for w in file_.readline().split()]
+                    l += 1
                 except:
                     print(f"ERR: Expected a number on line {l}")
+                    exit(1)
 
-                self.add_vertice(v, weight)
+                vertices.append((x, y))
+                self.add_vertice((x, y), weight)
                 for n in neighbours:
-                    self.add_edge(v, n)
-            break  # TODO: remove this
+                    if not n in range(0, len(vertices)):
+                        print(
+                            f"ERR: Expected a number between [{0},{len(vertices)}[ on line {l}")
+                        exit(1)
+                    self.add_edge((x, y), vertices[n])
+            break
         return self
 
-    def random_graph(self, size=15, seed=2):
-        random.seed(seed)
+    def random_graph(self, size, seed=None):
+        assert size <= 81, "Size must be lower than 81"
 
+        self.vertices.clear()
+        self.edges.clear()
+        rdm.seed(seed)
+
+        vertices = []
         for v in range(size):
-            self.add_vertice(v, random.randint(1, size//2))
+            while True:
+                x, y = (rdm.randint(1, 9), rdm.randint(1, 9))
+                if not (x, y) in self.vertices:
+                    break
+            vertices.append((x, y))
+            self.add_vertice((x, y), rdm.randint(1, 99))
 
         for v1 in range(size - 1):
-            for _ in range(random.randint(1, size // 2)):
-                v2 = random.randint(v1 + 1, size - 1)
-                self.add_edge(v1, v2)
+            for _ in range(rdm.randint(1, size // 2)):
+                v2 = rdm.randint(v1 + 1, size - 1)
+                self.add_edge(vertices[v1], vertices[v2])
 
         return self
 
@@ -101,22 +124,37 @@ class SearchGraph:
             norm=norm, cmap=mpl.cm.coolwarm)
         colors = [mapper.to_rgba(i)
                   for i in self.vertices.values()]
+        positions = {x: x for x in G.nodes}
+
+        sol_nodes = nx.draw_networkx_nodes(
+            G,
+            pos=positions,
+            nodelist=self.solution[1][0],
+            node_size=800,
+            node_color="white"
+        )
+        sol_nodes.set_edgecolor('black')
 
         nx.draw(G,
+                pos=positions,
+                labels=self.vertices,
                 nodelist=self.vertices,
                 cmap=mpl.cm.summer,
                 node_color=colors,
                 font_weight='bold',
-                font_color='white',
+                font_color='whitesmoke',
                 with_labels=True)
 
         plt.show()
 
-    def set_cost(self, subset):
+    def subset_cost(self, subset):
         return sum(self.vertices[v] for v in subset)
 
-    def is_dominating_set(self, subset, graph):
+    def is_dominating_subset(self, subset, graph):
         return all(self.edges[v] & subset for v in graph - subset)
+
+    def heuristic(self, v: Point, ):
+        return
 
     def exhaustive_combinations(self):
         sorted_weights = sorted(self.vertices.values())
@@ -129,14 +167,15 @@ class SearchGraph:
                 return
 
             for subset in itertools.combinations(range(self.size), sub_size):
-                # cost = self.set_cost(subset)
+                # cost = self.subset_cost(subset)
                 yield None, subset
 
     def greedy_combinations(self):
         sorted_vertices = [x for x, _ in sorted(
             self.vertices.items(), key=lambda x: x[1])]
         n = len(sorted_vertices)
-        queue = [(self.vertices[sorted_vertices[i]], (i,)) for i in reversed(range(n))]
+        queue = [(self.vertices[sorted_vertices[i]], (i,))
+                 for i in reversed(range(n))]
 
         while queue:
             cost, index = queue.pop()
@@ -153,30 +192,8 @@ class SearchGraph:
             yield cost, subset
 
             for i in reversed(range(index[-1] + 1, n)):
-                queue.append((cost + self.vertices[sorted_vertices[i]], (*index, i)))
-
-    def greedy_combinations2(self):
-        sorted_vertices = [x for x, _ in sorted(
-            self.vertices.items(), key=lambda x: (x[1], len(self.edges[x[0]])))]
-        n = len(sorted_vertices)
-        queue = [(self.vertices[sorted_vertices[i]], (i,)) for i in reversed(range(n))]
-
-        while queue:
-            cost, index = queue.pop()
-
-            if len(index) == n:
-                continue
-
-            subset = tuple(sorted_vertices[i] for i in index)
-
-            if cost > self.min_cost:
-                # prune
-                return
-
-            yield cost, subset
-
-            for i in reversed(range(index[-1] + 1, n)):
-                queue.append((cost + self.vertices[sorted_vertices[i]], (*index, i)))
+                queue.append(
+                    (cost + self.vertices[sorted_vertices[i]], (*index, i)))
 
     def astar_combinations(self):
         sorted_vertices = [x for x, _ in sorted(
@@ -205,30 +222,28 @@ class SearchGraph:
             return self.exhaustive_combinations()
         elif strategy == 'greedy':
             return self.greedy_combinations()
-        elif strategy == 'greedy2':
-            return self.greedy_combinations2()
         elif strategy == 'astar':
             return self.astar_combinations()
         else:
             assert False, f"Unrecognized strategy \"{strategy}\""
 
     def search(self, strategy):
-        graph = set(range(self.size))
+        graph = set(self.vertices)
 
-        self.min_cost = self.set_cost(graph)
+        self.min_cost = self.subset_cost(graph)
         self.min_subsets = [graph]
         self.iter = 0
-        start = timer()
+        start = perf_counter()
 
         for cost, subset in self.combinations(strategy):
             self.iter += 1
 
             subset = set(subset)
-            if not self.is_dominating_set(subset, graph):
+            if not self.is_dominating_subset(subset, graph):
                 continue
 
             if cost is None:
-                cost = self.set_cost(subset)
+                cost = self.subset_cost(subset)
 
             if cost < self.min_cost:
                 self.min_cost = cost
@@ -236,42 +251,26 @@ class SearchGraph:
             elif cost == self.min_cost:
                 self.min_subsets.append(subset)
 
-        self.time = timer() - start
+        self.exec_time = perf_counter() - start
 
 
-# g = SearchGraph().read_graph("graph2.txt")
-
-g = SearchGraph().random_graph(size=40, seed=93446)     # (280) 0.0002 0.0003 (65) 7.799954  14.140436
-# g = SearchGraph().random_graph(size=40, seed=234)       # (280) 0.0002 0.0003 (65) 2.540548  80.498313
-# g = SearchGraph().random_graph(size=25, seed=93302)     # (280) 0.0002 0.0003 (65) 12.649675 4.203344
-# g = SearchGraph().random_graph(size=30, seed=93015)     # (280) 0.0002 0.0003 (65) 5.463316  20.374070
-g = SearchGraph().random_graph(size=35, seed=3232)     # (280) 0.0002 0.0003 (65) 7.799954  14.140436
-g = SearchGraph().random_graph(size=70, seed=32424)     # (280) 0.0002 0.0003 (65) 7.799954  14.140436
-g = SearchGraph().random_graph(size=70, seed=13)     # (280) 0.0002 0.0003 (65) 7.799954  14.140436
-g = SearchGraph().random_graph(size=100, seed=5)     # (280) 0.0002 0.0003 (65) 7.799954  14.140436
-
-# g = SearchGraph().random_graph(size=16, seed=93446)
-
-g.draw_graph()
+# g = SearchGraph().read_graph("graph1.txt")
+g = SearchGraph().random_graph(20, 93446)
 
 g.search('greedy')
 print(g.solution, g.iter)
-print(f"{g.time:.6f}")
-
-g.search('greedy2')
-print(g.solution, g.iter)
-print(f"{g.time:.6f}")
-
-g.search('astar')
-print(g.solution, g.iter)
-print(f"{g.time:.6f}")
-
-g.search('exhaustive')
-print(g.solution, g.iter)
-print(f"{g.time:.6f}")
-
-
+print(f"{g.exec_time:.6f}")
 
 g.draw_graph()
 
-# http://webhome.cs.uvic.ca/~wendym/courses/445/14/445_heur.html
+g.search('astar')
+print(g.solution, g.iter)
+print(f"{g.exec_time:.6f}")
+
+g.draw_graph()
+
+g.search('exhaustive')
+print(g.solution, g.iter)
+print(f"{g.exec_time:.6f}")
+
+g.draw_graph()
