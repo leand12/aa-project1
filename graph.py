b50f46f8f95b971595c642c2a8586e6f178b9e26
leand12 <leandrosilva12@ua.pt>
2021-12-05 09:07:59 +0000

    plots + improvements

---
+++ graph.py
@@ -0,0 +1,302 @@
+import heapq
+import itertools
+import random as rdm
+import networkx as nx
+import matplotlib as mpl
+from time import perf_counter
+from typing import Tuple, Set, Iterable, Callable
+
+Point = Tuple[int, int]
+
+
+class SearchGraph:
+
+    def __init__(self):
+        self.vertices = {}
+        self.edges = {}
+        self.min_cost = None
+        self.min_subsets = None
+        self.exec_time = None
+
+    @property
+    def size(self):
+        return len(self.vertices)
+
+    @property
+    def solution(self):
+        return self.min_cost, self.min_subsets
+
+    def add_vertice(self, v: Point, w: int):
+        assert w >= 0, "Weight must be positive"
+        assert isinstance(v, tuple), f"Vertice must be a tuple"
+        assert v not in self.vertices, f"Vertice {v} already added"
+        assert len(self.vertices) <= 81, "Limit of vertices exceeded"
+
+        self.vertices[v] = w
+        return self
+
+    def add_edge(self, v1: Point, v2: Point):
+        for v in (v1, v2):
+            assert v in self.vertices, f"Vertice {v} does not exist"
+        assert not v1 == v2, f"Vertices {v1} {v2} are equal"
+
+        self.edges.setdefault(v1, set()).add(v2)
+        self.edges.setdefault(v2, set()).add(v1)
+        return self
+
+    def read_graph(self, filename: str):
+        self.vertices.clear()
+        self.edges.clear()
+
+        try:
+            file_ = open(filename, "r")
+        except FileNotFoundError:
+            print(f"ERR: File \"{filename}\" not found")
+            exit(1)
+
+        l = 0
+        while file_:
+            line = file_.readline()
+            l += 1
+            if not line:
+                continue
+            try:
+                num_vertices = int(line)
+            except:
+                print(f"ERR: Expected a number on line {l}")
+                exit(1)
+
+            vertices = []
+            for v in range(num_vertices):
+                try:
+                    x, y, weight, *neighbours = [int(w)
+                                                 for w in file_.readline().split()]
+                    l += 1
+                except:
+                    print(f"ERR: Expected a number on line {l}")
+                    exit(1)
+
+                vertices.append((x, y))
+                self.add_vertice((x, y), weight)
+                for n in neighbours:
+                    if not n in range(0, len(vertices)):
+                        print(
+                            f"ERR: Expected a number between [{0},{len(vertices)}[ on line {l}")
+                        exit(1)
+                    self.add_edge((x, y), vertices[n])
+            break
+        return self
+
+    def random_graph(self, size: int, seed: int = None):
+        assert 2 <= size <= 81, "Size must be between [2, 81]"
+
+        self.vertices.clear()
+        self.edges.clear()
+        rdm.seed(seed)
+
+        vertices = []
+        for v in range(size):
+            while True:
+                x, y = (rdm.randint(1, 9), rdm.randint(1, 9))
+                if not (x, y) in self.vertices:
+                    break
+            vertices.append((x, y))
+            self.add_vertice((x, y), rdm.randint(1, 99))
+
+        for v1 in range(size - 1):
+            for _ in range(rdm.randint(1, size // 2)):
+                v2 = rdm.randint(v1 + 1, size - 1)
+                self.add_edge(vertices[v1], vertices[v2])
+
+        return self
+
+    def draw_graph(self, ax=None):
+        G = nx.Graph()
+
+        for v, neighbours in self.edges.items():
+            for n in neighbours:
+                G.add_edge(v, n)
+
+        positions = {x: x for x in G.nodes}
+        low, *_, high = sorted(self.vertices.values())
+        norm = mpl.colors.Normalize(
+            vmin=low, vmax=high, clip=True)
+        mapper = mpl.cm.ScalarMappable(
+            norm=norm, cmap=mpl.cm.coolwarm)
+        colors = [mapper.to_rgba(i)
+                  for i in self.vertices.values()]
+
+        sol_vertices = {}
+        sol_colors = []
+        rem_vertices = {}
+        rem_colors = []
+        for i, (v, w) in enumerate(self.vertices.items()):
+            if v in self.solution[1][0]:
+                sol_vertices[v] = w
+                sol_colors.append(colors[i])
+            else:
+                rem_vertices[v] = w
+                rem_colors.append(colors[i])
+
+        nx.draw_networkx_edges(
+            G,
+            pos=positions,
+            nodelist=self.vertices,
+            edge_color='gray',
+            node_size=500,
+            width=2,
+            ax=ax)
+
+        nx.draw_networkx_nodes(
+            G,
+            pos=positions,
+            labels=sol_vertices,
+            nodelist=sol_vertices,
+            node_size=800,
+            node_color=sol_colors,
+            cmap=mpl.cm.summer,
+            ax=ax)
+
+        rem_nodes = nx.draw_networkx_nodes(
+            G,
+            pos=positions,
+            labels=rem_vertices,
+            nodelist=rem_vertices,
+            node_size=500,
+            node_color='white',
+            cmap=mpl.cm.summer,
+            ax=ax)
+        rem_nodes.set_edgecolors(rem_colors)
+        rem_nodes.set_linewidth(2)
+
+        nx.draw_networkx_labels(
+            G,
+            pos=positions,
+            labels=self.vertices,
+            font_weight='bold',
+            font_color='black',
+            with_labels=True)
+
+    def subset_cost(self, subset: Iterable[Point]):
+        return sum(self.vertices[v] for v in subset)
+
+    def is_dominating_subset(self, subset: Set[Point], graph: Set[Point]):
+        return all(self.edges[v] & subset for v in graph - subset)
+
+    def heuristic(self):
+        return [v for v, _ in sorted(
+            self.vertices.items(), key=lambda x: x[1])]
+
+    def heuristic2(self):
+        return [v for v, _ in sorted(
+            self.vertices.items(), key=lambda x: x[1] - len(self.edges(x[0])))]
+
+    def exhaustive_combinations(self, with_bnb=True):
+        sorted_weights = sorted(self.vertices.values())
+        cumulative_min_weight = [sum(sorted_weights[:s + 1])
+                                 for s in range(self.size)]
+
+        vertices = list(self.vertices.keys())
+
+        for sub_size in range(1, self.size + 1):
+            if with_bnb and cumulative_min_weight[sub_size - 1] > self.min_cost:
+                # prune: global minimum already found
+                return
+
+            for index in itertools.combinations(range(self.size), sub_size):
+                subset = set(vertices[i] for i in index)
+                # it is inefficient to calculate cost here
+                yield None, subset
+
+    def greedy_combinations(self, heuristic: Callable):
+        sorted_vertices = heuristic()
+        n = len(sorted_vertices)
+        queue = [(self.vertices[sorted_vertices[i]], (i,))
+                 for i in reversed(range(n))]
+
+        while queue:
+            cost, index = queue.pop()
+
+            subset = set(sorted_vertices[i] for i in index)
+
+            if cost > self.min_cost:
+                # prune: local minimum found
+                return
+
+            yield cost, subset
+
+            for i in reversed(range(index[-1] + 1, n)):
+                queue.append(
+                    (cost + self.vertices[sorted_vertices[i]], (*index, i)))
+
+    def astar_combinations(self, heuristic: Callable, with_heap: bool = True):
+        sorted_vertices = heuristic()
+        n = len(sorted_vertices)
+        queue = [(self.vertices[sorted_vertices[i]], (i,)) for i in range(n)]
+
+        while queue:
+            if with_heap:
+                cost, index = heapq.heappop(queue)
+            else:
+                # inversed order to pop the end of the list
+                # and avoid more computational costs
+                queue.sort(key=lambda x: -x[0])
+                cost, index = queue.pop()
+
+            subset = set(sorted_vertices[i] for i in index)
+
+            if cost > self.min_cost:
+                # prune: global minimum found
+                return
+
+            yield cost, subset
+
+            for i in range(index[-1] + 1, n):
+                node = (cost + self.vertices[sorted_vertices[i]], (*index, i))
+                if with_heap:
+                    heapq.heappush(queue, node)
+                else:
+                    queue.append(node)
+
+    def combinations(self, strategy: str, heuristic: str):
+        if strategy == 'exhaustive':
+            return self.exhaustive_combinations(False)
+        elif strategy == 'branch_and_bound':
+            return self.exhaustive_combinations()
+        elif strategy == 'greedy':
+            return self.greedy_combinations(
+                self.heuristic2 if heuristic == 2 else self.heuristic)
+        elif strategy == 'astar':
+            return self.astar_combinations(
+                self.heuristic2 if heuristic == 2 else self.heuristic, False)
+        elif strategy == 'astar_heap':
+            return self.astar_combinations(
+                self.heuristic())
+        else:
+            assert False, f"Unrecognized strategy \"{strategy}\""
+
+    def search(self, strategy: str, heuristic: int):
+        assert len(self.vertices) >= 2, "Not enough vertices to search"
+        graph = set(self.vertices)
+
+        self.min_cost = self.subset_cost(graph)
+        self.min_subsets = [graph]
+        self.basic_ops = 0
+        start = perf_counter()
+
+        for cost, subset in self.combinations(strategy, heuristic):
+            self.basic_ops += 1
+
+            if not self.is_dominating_subset(subset, graph):
+                continue
+
+            if cost is None:
+                cost = self.subset_cost(subset)
+
+            if cost < self.min_cost:
+                self.min_cost = cost
+                self.min_subsets = [subset]
+            elif cost == self.min_cost:
+                self.min_subsets.append(subset)
+
+        self.exec_time = perf_counter() - start
--- main.py
+++ main.py
@@ -1,316 +1,182 @@
-
-import itertools
-import random as rdm
-import networkx as nx
-import matplotlib as mpl
+import argparse
 import matplotlib.pyplot as plt
-from time import perf_counter
-import time
 from typing import Tuple
-
-Point = Tuple[int, int]
-
-
-class SearchGraph:
-
-    def __init__(self):
-        self.vertices = {}
-        self.edges = {}
-        self.min_cost = None
-        self.min_subsets = None
-        self.exec_time = None
-
-    @property
-    def size(self):
-        return len(self.vertices)
-
-    @property
-    def solution(self):
-        return self.min_cost, self.min_subsets
-
-    def add_vertice(self, v: Point, w: int):
-        assert w >= 0, "Weight must be positive"
-        assert isinstance(v, tuple), f"Vertice must be a tuple"
-        assert v not in self.vertices, f"Vertice {v} already added"
-        assert len(self.vertices) <= 81, "Limit of vertices exceeded"
-
-        self.vertices[v] = w
-        return self
-
-    def add_edge(self, v1: Point, v2: Point):
-        for v in (v1, v2):
-            assert v in self.vertices, f"Vertice {v} does not exist"
-        assert not v1 == v2, f"Vertices {v1} {v2} are equal"
-
-        self.edges.setdefault(v1, set()).add(v2)
-        self.edges.setdefault(v2, set()).add(v1)
-        return self
-
-    def read_graph(self, filename):
-        self.vertices.clear()
-        self.edges.clear()
-
-        try:
-            file_ = open(filename, "r")
-        except FileNotFoundError:
-            print(f"ERR: File \"{filename}\" not found")
-            exit(1)
-
-        l = 0
-        while file_:
-            line = file_.readline()
-            l += 1
-            if not line:
-                continue
-            try:
-                num_vertices = int(line)
-            except:
-                print(f"ERR: Expected a number on line {l}")
-                exit(1)
-
-            vertices = []
-            for v in range(num_vertices):
-                try:
-                    x, y, weight, *neighbours = [int(w)
-                                                 for w in file_.readline().split()]
-                    l += 1
-                except:
-                    print(f"ERR: Expected a number on line {l}")
-                    exit(1)
-
-                vertices.append((x, y))
-                self.add_vertice((x, y), weight)
-                for n in neighbours:
-                    if not n in range(0, len(vertices)):
-                        print(
-                            f"ERR: Expected a number between [{0},{len(vertices)}[ on line {l}")
-                        exit(1)
-                    self.add_edge((x, y), vertices[n])
-            break
-        return self
-
-    def random_graph(self, size, seed=None):
-        assert 2 <= size <= 81, "Size must be between [2, 81]"
-
-        self.vertices.clear()
-        self.edges.clear()
-        rdm.seed(seed)
-
-        vertices = []
-        for v in range(size):
-            while True:
-                x, y = (rdm.randint(1, 9), rdm.randint(1, 9))
-                if not (x, y) in self.vertices:
-                    break
-            vertices.append((x, y))
-            self.add_vertice((x, y), rdm.randint(1, 99))
-
-        for v1 in range(size - 1):
-            for _ in range(rdm.randint(1, size // 2)):
-                v2 = rdm.randint(v1 + 1, size - 1)
-                self.add_edge(vertices[v1], vertices[v2])
-
-        return self
-
-    def draw_graph(self, ax=None):
-        G = nx.Graph()
-
-        for v, neighbours in self.edges.items():
-            for n in neighbours:
-                G.add_edge(v, n)
-
-        positions = {x: x for x in G.nodes}
-        low, *_, high = sorted(self.vertices.values())
-        norm = mpl.colors.Normalize(
-            vmin=low, vmax=high, clip=True)
-        mapper = mpl.cm.ScalarMappable(
-            norm=norm, cmap=mpl.cm.coolwarm)
-        colors = [mapper.to_rgba(i)
-                  for i in self.vertices.values()]
-
-        sol_vertices = {}
-        sol_colors = []
-        rem_vertices = {}
-        rem_colors = []
-        for i, (v, w) in enumerate(self.vertices.items()):
-            if v in self.solution[1][0]:
-                sol_vertices[v] = w
-                sol_colors.append(colors[i])
-            else:
-                rem_vertices[v] = w
-                rem_colors.append(colors[i])
-
-        nx.draw_networkx_edges(
-            G,
-            pos=positions,
-            nodelist=self.vertices,
-            edge_color='gray',
-            node_size=500,
-            width=2,
-            ax=ax)
-
-        nx.draw_networkx_nodes(
-            G,
-            pos=positions,
-            labels=sol_vertices,
-            nodelist=sol_vertices,
-            node_size=800,
-            node_color=sol_colors,
-            cmap=mpl.cm.summer,
-            ax=ax)
-
-        rem_nodes = nx.draw_networkx_nodes(
-            G,
-            pos=positions,
-            labels=rem_vertices,
-            nodelist=rem_vertices,
-            node_size=500,
-            node_color='white',
-            cmap=mpl.cm.summer,
-            ax=ax)
-        rem_nodes.set_edgecolors(rem_colors)
-        rem_nodes.set_linewidth(2)
-
-        nx.draw_networkx_labels(
-            G,
-            pos=positions,
-            labels=self.vertices,
-            font_weight='bold',
-            font_color='black',
-            with_labels=True)
-
-    def subset_cost(self, subset):
-        return sum(self.vertices[v] for v in subset)
-
-    def is_dominating_subset(self, subset, graph):
-        return all(self.edges[v] & subset for v in graph - subset)
-
-    def heuristic(self, v: Point, ):
-        return
-
-    def exhaustive_combinations(self):
-        sorted_weights = sorted(self.vertices.values())
-        cumulative_min_weight = [sum(sorted_weights[:s + 1])
-                                 for s in range(self.size)]
-
-        vertices = list(self.vertices.keys())
-
-        for sub_size in range(1, self.size + 1):
-            if cumulative_min_weight[sub_size - 1] > self.min_cost:
-                # prune
-                return
-
-            for index in itertools.combinations(range(self.size), sub_size):
-                # cost = self.subset_cost(subset)
-                subset = set(vertices[i] for i in index)
-                yield None, subset
-
-    def greedy_combinations(self):
-        sorted_vertices = [x for x, _ in sorted(
-            self.vertices.items(), key=lambda x: x[1])]
-        n = len(sorted_vertices)
-        queue = [(self.vertices[sorted_vertices[i]], (i,))
-                 for i in reversed(range(n))]
-
-        while queue:
-            cost, index = queue.pop()
-
-            subset = set(sorted_vertices[i] for i in index)
-
-            if cost > self.min_cost:
-                # prune
-                return
-
-            yield cost, subset
-
-            for i in reversed(range(index[-1] + 1, n)):
-                queue.append(
-                    (cost + self.vertices[sorted_vertices[i]], (*index, i)))
-
-    def astar_combinations(self):
-        sorted_vertices = [x for x, _ in sorted(
-            self.vertices.items(), key=lambda x: x[1])]
-        n = len(sorted_vertices)
-        queue = [(self.vertices[sorted_vertices[i]], (i,)) for i in range(n)]
-
-        while queue:
-            queue.sort(key=lambda x: -x[0])
-            cost, index = queue.pop()
-
-            subset = set(sorted_vertices[i] for i in index)
-
-            if cost > self.min_cost:
-                # prune
-                return
-
-            yield cost, subset
-
-            for i in range(index[-1] + 1, n):
-                queue.append(
-                    (cost + self.vertices[sorted_vertices[i]], (*index, i)))
-
-    def combinations(self, strategy):
-        if strategy == 'exhaustive':
-            return self.exhaustive_combinations()
-        elif strategy == 'greedy':
-            return self.greedy_combinations()
-        elif strategy == 'astar':
-            return self.astar_combinations()
-        else:
-            assert False, f"Unrecognized strategy \"{strategy}\""
-
-    def search(self, strategy):
-        assert len(self.vertices) >= 2, "Not enough vertices to search"
-        graph = set(self.vertices)
-
-        self.min_cost = self.subset_cost(graph)
-        self.min_subsets = [graph]
-        self.iter = 0
-        start = perf_counter()
-
-        for cost, subset in self.combinations(strategy):
-            self.iter += 1
-
-            if not self.is_dominating_subset(subset, graph):
-                continue
-
-            if cost is None:
-                cost = self.subset_cost(subset)
-
-            if cost < self.min_cost:
-                self.min_cost = cost
-                self.min_subsets = [subset]
-            elif cost == self.min_cost:
-                self.min_subsets.append(subset)
-
-        self.exec_time = perf_counter() - start
-
-
-# g = SearchGraph().read_graph("graph1.txt")
-
-for size in range(29, 40):
-    g = SearchGraph().random_graph(size, 93446)
-
-    fig, ax = plt.subplots(1, 3, num=1)
-    fig.set_size_inches(21, 7)
-
-    for i, algo in enumerate(('greedy', 'astar', 'exhaustive')):
-
-        g.search(algo)
-        print(g.solution, g.iter)
-        print(f"{g.exec_time:.6f}")
-
-        plt.sca(ax[i])
-        g.draw_graph(ax[i])
-
-        ax[i].set_title(
-            f"Solution with {algo.capitalize()} Search and size = {size}", fontsize=14)
-        ax[i].set_xlabel(f"total cost = {g.solution[0]}", fontsize=12)
-        ax[i].set_axis_on()
-        ax[i].set_xlim(0, 10)
-        ax[i].set_ylim(0, 10)
-        ax[i].tick_params(left=True, bottom=True,
-                          labelleft=True, labelbottom=True)
-
-    fig.savefig(f"plots/g{size}.png", dpi=100)
-    fig.clear()
+from graph import SearchGraph
+
+parser = argparse.ArgumentParser(
+    usage="main.py [-h] (-f FILE | -r SEED) [-s N] [-a NAME] [-hr N]",
+    description='Program that solves the Minimum-Weight Dominating Set Problem')
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-f', '--read-file', metavar='FILE', type=argparse.FileType('r'),
+                   help='create a graph based on a correct format inside a file')
+group.add_argument('-r', '--create-random', metavar='SEED', type=int,
+                   help='create a random graph based on a seed')
+parser.add_argument('-s', '--size', metavar='N', default=25, type=int, required=False,
+                    help='the number of vertices of the graph (default: %(default)s)')
+parser.add_argument('-a', '--algorithm', metavar='NAME', default='exhaustive', type=str, required=False,
+                    choices=['exhaustive', 'branch_and_bound', 'greedy', 'astar', 'astar_heap'],
+                    help='the number of vertices of the graph (default: %(default)s)')
+parser.add_argument('-hr', '--heuristic', metavar='N', default=1, type=int, required=False,
+                    help='the heuristic used by the Greedy and A-star approach: '
+                    '(1) based on weights, (2) based on weights-degree, (default: %(default)s)')
+
+args = vars(parser.parse_args())
+
+
+def plot_basic_ops(sizes: Tuple[int, int], algos: Tuple[str, ...]):
+    data = []
+    print(f" Nodes & {' & '.join(a.title() for a in algos)}")
+
+    for size in range(*sizes):
+        g = SearchGraph().random_graph(size, 93446)
+        data.append([])
+
+        for algo in algos:
+            g.search(algo)
+            data[-1].append(g.basic_ops)
+
+        print(f" {size}", end='')
+        for d in data[-1]:
+            print(f" & {d:d}", end='')
+        print()
+
+    # data.extend(
+    #     [[15, 25387, 6474540],
+    #     [13, 1779, 174436],
+    #     [8, 557, 36456],
+    #     [17, 3033, 1149016],
+    #     [15, 5680, 1391841],
+    #     [26, 28766, 25211935],
+    #     [8, 2600, 2007327],
+    #     [7, 520, 66711],
+    #     [8, 236, 74518],
+    #     [14, 11852, 15965871],
+    #     [10, 5839, 3930550],
+    #     [6, 627, 760098],
+    #     [18, 1063, 5358577],
+    #     [14, 9647, 33199095],
+    #     [18, 5432, 39419863],
+    #     [9, 7332, 46615613],
+    #     [19, 15109, 54910659]]
+    # )
+
+    plt.yscale('log')
+    plt.plot(range(*sizes), data)
+    plt.title("Variation of the number of Operations with the Graph Size")
+    plt.legend([a.title() for a in algos])
+    plt.xlabel("Graph # of nodes")
+    plt.ylabel("Basic operations log(ops)")
+    plt.savefig("plots/basic_ops.png")
+    plt.cla()
+    plt.clf()
+
+
+def plot_exec_s(sizes: Tuple[int, int], algos: Tuple[str, ...]):
+    data = []
+    print(f" nodes & {' & '.join(a.title() for a in algos)}")
+
+    for size in range(*sizes):
+        g = SearchGraph().random_graph(size, 93446)
+        data.append([])
+
+        for algo in algos:
+            g.search(algo)
+            data[-1].append(g.exec_)
+
+        print(f" {size}", end='')
+        for d in data[-1]:
+            print(f" & {d*100:.3f}", end='')
+        print()
+
+    # data.extend(
+    #     [[0.000379, 550.962854, 23.488071],
+    #      [0.001075, 2.144161, 0.492601],
+    #         [0.000152, 0.240921, 0.082189],
+    #         [0.000272, 6.184916, 4.235634],
+    #         [0.000279, 25.116781, 4.580443],
+    #         [0.000524, 761.308984, 131.663930],
+    #         [0.000223, 5.477160, 8.216070],
+    #         [0.000191, 0.281841, 0.250342],
+    #         [0.000217, 0.070878, 0.285507],
+    #         [0.000314, 188.231096, 70.799879],
+    #         [0.000269, 45.391021, 17.147758],
+    #         [0.000152, 0.374619, 2.844985],
+    #         [0.000420, 1.078891, 27.393365],
+    #         [0.000382, 152.553328, 221.819140],
+    #         [0.000454, 47.667561, 276.248185],
+    #         [0.000244, 91.863296, 325.867944],
+    #         [0.000511, 427.059270, 391.750860]]
+    # )
+    plt.yscale('log')
+    plt.plot(range(*sizes), data)
+    plt.title("Variation of the Execution  with the Graph Size")
+    plt.legend([a.title() for a in algos])
+    plt.xlabel("Graph # of nodes")
+    plt.ylabel("Execution  log(s)")
+    plt.savefig("plots/exec_s.png")
+    plt.cla()
+    plt.clf()
+
+
+def plot_network_solutions(sizes: Tuple[int, int], algos: Tuple[str, ...]):
+
+    for size in range(*sizes):
+        g = SearchGraph().random_graph(size, 93446)
+
+        fig, ax = plt.subplots(1, 3, num=1)
+        fig.set_size_inches(21, 7)
+
+        for i, algo in enumerate(algos):
+
+            g.search(algo)
+            print(g.solution, g.basic_ops, f"{g.exec_:.6f}")
+
+            plt.sca(ax[i])
+            g.draw_graph(ax[i])
+
+            ax[i].set_title(
+                f"Solution with {algo.title()} Search and size = {size}", fontsize=14)
+            ax[i].set_xlabel(f"total cost = {g.solution[0]}", fontsize=12)
+            ax[i].set_axis_on()
+            ax[i].set_xlim(0, 10)
+            ax[i].set_ylim(0, 10)
+            ax[i].tick_params(left=True, bottom=True,
+                              labelleft=True, labelbottom=True)
+
+        fig.savefig(f"plots/g{size}.png", dpi=100)
+        fig.clear()
+
+
+if __name__ == '__main__':
+    # g = SearchGraph().read_graph("graph1.txt")
+
+    # plot_basic_ops((2, 46), ('greedy', 'astar_heap', 'exhaustive'))
+    # plot_exec_s((2, 46), ('greedy', 'astar_heap', 'exhaustive'))
+    # plot_network_solutions((2, 45), ('greedy', 'astar', 'exhaustive'))
+
+    seed = args["create_random"]
+    size = args["size"]
+    algo = args["algorithm"]
+    heur = args["heuristic"]
+    
+    if seed:
+        g = SearchGraph().random_graph(size, seed)
+    else:
+        g = SearchGraph().read_graph(args["read_file"].name)
+
+    g.search(algo, heur)
+    g.draw_graph()
+
+    print('Solution:', g.solution)
+    print(f"({g.exec_time:.6f} seconds, {g.basic_ops} basic ops)")
+
+    plt.title(
+        f"Solution with {algo.title()} Search and size = {size}", fontsize=14)
+    plt.xlabel(f"total cost = {g.solution[0]}", fontsize=12)
+    plt.axis('on')
+    plt.xlim(0, 10)
+    plt.ylim(0, 10)
+    plt.tick_params(left=True, bottom=True,
+                    labelleft=True, labelbottom=True)
+    plt.show()
